b3e9e6768121d4f29635d0f200fffcdcaa6266fd
Nuha <100066558@ku.ac.ae>
2025-10-01 16:26:33 +0400

    Fix calculator

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 def do(a,b,o):
-    if o=="add":
+    if o=="+":
         return a+b
-    if o=="sub":
+    if o=="-":
         return a-b
-    if o=="mult":
+    if o=="x":
         return a*b
-    if o=="div":
+    if o=="/":
         if b==0:
             print("error - cannot divide by 0") 
             return 0
@@ -17,4 +17,4 @@ print("CALCULATOR")
 x=input("Value 1: ")
 y=input("Value 2: ")
 z=input("Operation (+,-,x,/): ")
-print("Result: ",do(int(x),int(y),z))
+print("Result:",do(int(x),int(y),z))
